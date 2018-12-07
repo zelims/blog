099fac732aa807ac7df835c079b7399a581436b1
zelims <zelims@users.noreply.github.com>
2018-12-06 18:51:08 -0700

    Pagination prev and next buttons function

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,17 +1,55 @@
-$('a.page-link').click(function(e)
+$('a.page-number').click(function(e)
 {
-    $('li.page-item.active').removeClass('active');
-    $(this).parent().addClass('active');
     let page = $(this).attr('page-number');
+
+    switchPage(page);
+
+    return false;
+});
+
+$('li#nextPage a').click(function(e) {
+    let nextPage = +getCurrentPage() + 1;
+    switchPage(nextPage);
+});
+$('li#prevPage a').click(function(e) {
+    let prevPage = +getCurrentPage() - 1;
+    switchPage(prevPage);
+});
+
+function getPageCount() {
+    return $('#postPageCount').val();
+}
+
+function getCurrentPage() {
+    return $('#curPage').val();
+}
+
+function switchPage(page) {
+    $('li.page-item.active').removeClass('active');
+    $('[page-number="'+page+'"]').parent().addClass('active');
+    let pageCount = getPageCount();
+    let prevPageBtn = $('#prevPage');
+    let nextPageBtn = $('#nextPage');
+
+    if(page == 1) {
+        prevPageBtn.addClass('disabled');
+    } else {
+        prevPageBtn.removeClass('disabled');
+    }
+    if(page == pageCount) {
+        nextPageBtn.addClass('disabled');
+    } else {
+        nextPageBtn.removeClass('disabled');
+    }
     $.ajax({
         url: "/page/" + page,
         method: "post",
         success: function(data) {
+            $('#curPage').val(page);
             $('#post-list').html(data);
         },
         error: function(err, t, s) {
             alert("Failed to get data: " + err.responseText);
         }
     });
-    return false;
-});
+}
