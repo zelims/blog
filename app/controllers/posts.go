cf54e153aeaf634baf0e71e8bd0d4f09b7f8b9bf
zelims <zelims@users.noreply.github.com>
2018-12-11 00:39:24 -0700

    View posts with option to edit via button

--- app/controllers/posts.go
+++ app/controllers/posts.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	"github.com/zelims/blog/app/models"
+)
 
 type Posts struct {
 	*revel.Controller
@@ -8,8 +11,13 @@ type Posts struct {
 }
 
 func (p Posts) View() revel.Result {
+	p.ViewArgs["posts"] = models.AllPosts()
 	return p.checkAuth("Manage/Posts/view.html")
 }
 func (p Posts) New() revel.Result {
 	return p.checkAuth("Manage/Posts/new.html")
+}
+
+func (p Posts) Edit(id int) revel.Result {
+	return p.checkAuth("Manage/Posts/edit.html")
 }
