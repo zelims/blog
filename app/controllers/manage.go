e8e6ad363c7b85d726ea0b13961ab828151c3ef2
zelims <zelims@users.noreply.github.com>
2018-12-19 15:36:26 -0700

    Adds dummy data to map for PoC of map

--- app/controllers/manage.go
+++ app/controllers/manage.go
@@ -13,6 +13,12 @@ type Manage struct {
 
 func (c Manage) Index() revel.Result {
 	c.ViewArgs["postCount"] = models.SizeOfAllPosts()
+	c.ViewArgs["visitors"] = map[string]int {
+		"AF": 16,
+		"AL": 11,
+		"DZ": 158,
+		"US": 1395,
+	}
 	return c.checkAuth("Manage/index.html")
 }
 
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,14 @@
-$('a.page-number').click(function(e)
-{
+// Show dropdown on hover
+$('.navbar a[data-toggle="dropdown"]').on("mouseover", function() {
+    $(this).next('div.dropdown-menu').addClass('show');
+});
+$('.navbar .dropdown-menu').on("mouseleave", function() {
+    $(this).removeClass('show');
+});
+
+$('a.page-number').click(function(e) {
     let page = $(this).attr('page-number');
-
     switchPage(page);
-
     return false;
 });
 
@@ -54,6 +59,55 @@ function switchPage(page) {
     });
 }
 
+let _markers = [];
+function createMap(element, markers="", seriesdata="") {
+    if(markers !== "")
+        _markers = markers;
+    element.vectorMap({
+        map: 'world_mill',
+        scaleColors: ['#C8EEFF', '#0071A4'],
+        normalizeFunction: 'polynomial',
+        zoomButtons: false,
+        zoomOnScroll: false,
+        panOnDrag: false,
+        hoverOpacity: 0.7,
+        hoverColor: false,
+        markerStyle: {
+            initial: {
+                fill: '#7E57C2',
+                stroke: '#383f47',
+                r: 6.5,
+            }
+        },
+        series: {
+            regions: [{
+                values: seriesdata,
+                scale: ['#bf9b9a', '#bf4f50'],
+                normalizeFunction: 'polynomial'
+            }]
+        },
+        onRegionTipShow: function(e, el, index){
+            if(seriesdata[index] != null)
+                el.html(el.html() + ' - ' + seriesdata[index]);
+            else
+                el.html(el.html())
+        },
+        backgroundColor: 'transparent',
+        regionStyle: {
+            initial: {
+                fill: '#8d8d8d'
+            }
+        },
+        //markers: _markers,
+        onRegionOver: function(e, tip, code) {
+            e.preventDefault();
+        }
+    });
+
+    let serverMapSvg = element.next('.jvectormap-container svg');
+    serverMapSvg.removeAttr('width');
+    serverMapSvg.removeAttr('height');
+}
 /**
  * Show Notification at top of screen
  * @param parent        (null)              =   parent div (a, btn, etc.)
